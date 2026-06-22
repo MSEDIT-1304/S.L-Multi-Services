1dcce0cb0c29b8ffec4b3e0fbe84ebeed1b6053c
MSEDIT-1304 <mairy.gillen@gmail.com>
2026-06-22 15:30:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def accueil():
+    return render_template("accueil.html")
+
+@app.route("/")
 def services():
     return render_template("services.html")
 
